8252cd665d515d8a93234937bcfb1b2087c342dc
jjscontrol64-gif <jjscontrol64@gmail.com>
2026-05-22 22:58:21 +0900

    init learn

--- src/main/java/com/jjs/patentapi/exception/PatentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.jjs.patentapi.exception;
-
-public class PatentNotFoundException extends RuntimeException {
-
-    public PatentNotFoundException(String id) {
-        super("Patent not found: " + id);
-    }
-}
