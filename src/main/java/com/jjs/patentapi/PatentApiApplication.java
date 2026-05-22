1876dc686b2b5f3d4bba153ddf39e4cee41b3ffc
jjscontrol64-gif <jjscontrol64@gmail.com>
2026-05-23 01:45:29 +0900

    feat architecture

---
+++ src/main/java/com/jjs/patentapi/PatentApiApplication.java
@@ -0,0 +1,11 @@
+package com.jjs.patentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatentApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PatentApiApplication.class, args);
+    }
+}
